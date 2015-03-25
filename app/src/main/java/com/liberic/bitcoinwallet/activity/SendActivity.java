ffbce96a5121e4e3cd34c8817339cb9bf6a25b50
Josrom <jvortsromero@gmail.com>
2015-03-25 15:17:06 +0100

    Add search in list of contacts and scan QR code

--- app/src/main/java/com/liberic/bitcoinwallet/activity/SendActivity.java
+++ app/src/main/java/com/liberic/bitcoinwallet/activity/SendActivity.java
@@ -1,15 +1,28 @@
 package com.liberic.bitcoinwallet.activity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.CommonDataKinds.Photo;
+import android.provider.ContactsContract.Data;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.zxing.client.android.Intents;
 import com.liberic.bitcoinwallet.R;
 import com.liberic.bitcoinwallet.adapter.ContactsAdapter;
 import com.liberic.bitcoinwallet.model.Contact;
@@ -30,33 +43,43 @@ public class SendActivity extends ActionBarActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.list_contacts);
-
-        List<Contact> data = new ArrayList<>();
-        Cursor mCursor = getContentResolver().query(
-                ContactsContract.Data.CONTENT_URI,
-                new String[] { ContactsContract.Data._ID, ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Photo.PHOTO },
-                ContactsContract.Data.MIMETYPE + " = '" + ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "' AND " + ContactsContract.CommonDataKinds.Phone.NUMBER + " IS NOT NULL",
-                null, ContactsContract.Data.DISPLAY_NAME + " ASC"
-        );
-
-        if(mCursor.moveToFirst()) {
-            do{
-                data.add(new Contact(mCursor.getString(mCursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME)),mCursor.getString(mCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))));
-            }while (mCursor.moveToNext());
-        }
-        mCursor.close();
-
-        data.add(new Contact("Test", "987813414"));
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mRecyclerView.setAdapter(new ContactsAdapter(data));
+        listQueryContact("");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_send, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        final SearchView mySearch = new SearchView(this);
+
+        // Assumes current activity is the searchable activity
+        mySearch.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        // Actions for search
+        mySearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                mySearch.clearFocus();
+                listQueryContact(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                listQueryContact(s);
+                return false;
+            }
+        });
+
+        menu.findItem(R.id.action_search).setActionView(mySearch);
+
         return true;
     }
 
@@ -69,6 +92,11 @@ public class SendActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_qrcode) {
+            openQrCamera();
+            return true;
+        }
+
+        if (id == R.id.action_search) {
             return true;
         }
 
@@ -77,5 +105,67 @@ public class SendActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void listQueryContact(String search){
+        List<Contact> data = new ArrayList<>();
+        Cursor mCursor = getContentResolver().query(
+                Data.CONTENT_URI,
+                new String[] { Data._ID, Data.DISPLAY_NAME, Phone.NUMBER, Phone.TYPE, Photo.PHOTO },
+                Data.MIMETYPE + " = '" + Phone.CONTENT_ITEM_TYPE + "' AND " + Phone.NUMBER + " IS NOT NULL AND " + Data.DISPLAY_NAME + " LIKE '%" + search + "%'",
+                null, Data.DISPLAY_NAME + " ASC"
+        );
+
+        if(mCursor.moveToFirst()) {
+            do{
+                data.add(new Contact(mCursor.getString(mCursor.getColumnIndex(Data.DISPLAY_NAME)),mCursor.getString(mCursor.getColumnIndex(Phone.NUMBER))));
+            }while (mCursor.moveToNext());
+        }
+        mCursor.close();
+
+        mRecyclerView.swapAdapter(new ContactsAdapter(data), false);
+    }
+
+    private void openQrCamera(){
+        try {
+            Intent intent = new Intent(Intents.Scan.ACTION);
+            intent.putExtra("SCAN_MODE","QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException anfe) {
+            //on catch, show the download dialog
+            showDialog(SendActivity.this, "No Scanner Found", "Download a scanner code activity?", "Yes", "No").show();
+        }
+    }
+
+    //alert dialog for downloadDialog
+    private static AlertDialog showDialog(final Activity act, CharSequence title, CharSequence message, CharSequence buttonYes, CharSequence buttonNo) {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(act);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Uri uri = Uri.parse("market://search?q=pname:" + "com.google.zxing.client.android");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    act.startActivity(intent);
+                } catch (ActivityNotFoundException anfe) {
+                }
+            }
+        });
+        downloadDialog.setNegativeButton(buttonNo, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }});
+        return downloadDialog.show();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
     }
 }
